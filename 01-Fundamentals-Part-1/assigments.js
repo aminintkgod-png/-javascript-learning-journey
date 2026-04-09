b742661f9250eb600dc11ba379a19029319f2051
Mohammad Amin <godngulove@gmail.com>
2026-04-09 21:52:52 +0900

    Initial commit - fundamentals setup

---
+++ 01-Fundamentals-Part-1/assigments.js
@@ -0,0 +1,55 @@
+// ASSIGMENT 1
+
+/*
+const country = 'JAPAN'
+const continent = 'ASIA'
+const population = 150
+
+console.log(country)
+console.log(continent)
+console.log(150)
+*/
+
+// ASSIGMENT 2
+/*
+const isIsland = true
+let population = 150
+const country = 'JAPAN'
+let language
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language)
+*/
+
+// ASSIGMENT 3
+/*
+
+const isIsland = true
+let population = 150
+const country = 'JAPAN'
+const language = 'Japanese'
+
+language = 'English'
+
+*/
+
+// Assigment 4
+let population = 150
+const country = 'JAPAN'
+const language = 'Japanese'
+// language = 'English'
+const continent = 'ASIA'
+let description =
+  country +
+  ' is in ' +
+  continent +
+  ', ' +
+  'and its ' +
+  population +
+  ' million people speak ' +
+  language
+
+console.log(population / 2)
+console.log(population++)
+console.log(population > 6)
+console.log(population < 33)
+console.log(description)
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,77 @@
+/*
+let js = 'amazing'
+
+console.log('Amin')
+console.log(26)
+
+// Declaring variable
+let firstName = 'amin'
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+*/
+
+/*
+true
+console.log(true)
+
+let javascriptIsFun = true
+
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log(typeof javascriptIsFun)
+console.log(typeof 23)
+console.log(typeof 'true')
+
+javascriptIsFun = 'YES'
+
+console.log(typeof javascriptIsFun)
+
+let year
+
+console.log(year, typeof year)
+
+year = 1999
+console.log(year, typeof year)
+
+console.log(typeof null)
+const birthYear = 1999
+
+
+const now = 2037
+
+const ageAmin = now - 1999
+const ageSarah = now - 1989
+console.log(ageAmin, ageSarah)
+
+console.log(ageAmin * 2, ageAmin / 10, ageAmin ** 3)
+
+const firstName = 'Mohammad Amin'
+const lastName = 'Gharibshah'
+console.log(firstName + ' ' + lastName)
+
+let x = 10 + 5
+console.log(x)
+
+x += 10 // x = x + 10
+
+x *= 4
+x++ // x = x + 1
+x--
+x--
+
+// Comparison operators
+console.log(ageAmin > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+*/
+
+const now = 2037
+
+const ageAmin = now - 1999
+const ageSarah = now - 1989
+
+console.log(now - 1999 > now - 1989)
